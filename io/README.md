97a52494f66f1b032548004ba6672e72aa0aaf8e
sanzhixiong86 <315570835@qq.com>
2020-05-23 17:26:39 +0800

    增加了txt的中文操作

--- io/README.md
+++ io/README.md
@@ -43,3 +43,40 @@ public class Test {
 
 读取操作
 
+```java
+package com.joy;
+import java.io.*;
+public class Test {
+    public static void main(String[] args) {
+        InputStream inputStream = null;
+        File file = null;
+        try
+        {
+            file = new File("111.txt");
+            try{
+              //这里是核心
+                inputStream = new FileInputStream(file);
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = inputStream.read(bytes)) != -1) {
+                    System.out.println(new String(bytes,0,len));
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                inputStream.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+```
--- io/code/src/com/joy/Test.java
+++ io/code/src/com/joy/Test.java
@@ -4,27 +4,35 @@ import java.io.*;
 
 public class Test {
     public static void main(String[] args) {
-        //file基础使用
-        File file = new File("./111.txt");
-        OutputStream outputStream = null;
         InputStream inputStream = null;
-        try {
-            String str = "sanzhixiong";//写入对应的数据
-            outputStream = new FileOutputStream(file);
-            outputStream.write(str.getBytes());
-            inputStream = new FileInputStream(file);
-            byte[] bytes = new byte[1024];
-            int len = 0;
-            String msg = null;
-            while ((len = inputStream.read(bytes)) != -1) {
-                msg = new String(bytes,0,len);
+        File file = null;
+        try
+        {
+            file = new File("111.txt");
+            try{
+                inputStream = new FileInputStream(file);
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = inputStream.read(bytes)) != -1) {
+                    System.out.println(new String(bytes,0,len));
+                }
             }
-            System.out.println("读取相对应的数据来"+msg);
-            inputStream.close();
-            outputStream.close();
+            catch (Exception e){
+                e.printStackTrace();
+            }
         }
         catch (Exception e){
             e.printStackTrace();
         }
+        finally {
+            try{
+                inputStream.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
