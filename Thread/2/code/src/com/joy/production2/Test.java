7d27aacc5908601b655daedb6729efaa573064ec
sanzhixiong86 <315570835@qq.com>
2020-06-01 09:35:58 +0800

    增加了三个生产者10个消费者的案例

--- Thread/2/code/src/com/joy/production2/Test.java
+++ Thread/2/code/src/com/joy/production2/Test.java
@@ -7,14 +7,15 @@ import java.util.concurrent.BlockingQueue;
 
 public class Test {
     public static void main(String[] args) {
-        BlockingQueue blockingQueue = new ArrayBlockingQueue(30); //5是个数
+        BlockingQueue blockingQueue = new ArrayBlockingQueue(5); //5是个数
         BreadProductionQeue breadProductionQeue = new BreadProductionQeue(blockingQueue);
         NoodleProdQueue noodleProdQueue = new NoodleProdQueue(blockingQueue);
         YoghurtProdQueue yoghurtProdQueue = new YoghurtProdQueue(blockingQueue);
-        ConsumerQueue consumerQueue = new ConsumerQueue(blockingQueue);
+        ConsumerQueue consumerQueue1 = new ConsumerQueue(blockingQueue);
+        new Thread(consumerQueue1).start();
         new Thread(breadProductionQeue).start();
         new Thread(noodleProdQueue).start();
         new Thread(yoghurtProdQueue).start();
-        new Thread(consumerQueue).start();
+
     }
 }
