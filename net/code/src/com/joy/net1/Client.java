fff3a7c4119c00e839b9f6a9816048f81b2523b2
sanzhixiong86 <315570835@qq.com>
2020-06-04 17:00:37 +0800

    修改客户端的操作

--- net/code/src/com/joy/net1/Client.java
+++ net/code/src/com/joy/net1/Client.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
         //客户端操作
-         Socket socket = new Socket("127.0.0.1",10000);
+         Socket socket = new Socket("localhost",10000);
          //发送数据
         OutputStream outputStream = socket.getOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
@@ -16,7 +16,7 @@ public class Client {
         InputStream inputStream = socket.getInputStream();
         DataInputStream dataInputStream = new DataInputStream(inputStream);
         byte[] buf = new byte[1024];
-        int read = dataInputStream.read();
+        int read = dataInputStream.read(buf);
         System.out.println(new String(buf,0,read));
         //关闭
         outputStream.close();
